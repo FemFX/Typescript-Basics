a1b361e64f9cccf200ab9c7e6be58515a3fe17e9
FemFX <ignedko34@gmail.com>
2019-11-04 18:05:54 +0300

    TS Functions

---
+++ functions.ts
@@ -0,0 +1,12 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+function square(a: number): number {
+  return a ** 2;
+}
+function sayHello(name: string): void {
+  console.log(`Hello ${name}`);
+}
+function someFunc(a?: any): void {
+  console.log(a);
+}
